34fac7079086bee99debac9c742dabd656a14367
guilherme <guilherme.cardosocruz@hotmail.com>
2026-02-03 09:11:47 -0300

    feat(racha): auto adicionar organizador como participante e campo isActive

---
+++ prisma/migrations/20260203120241_add_posteventparticipant_is_active/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isActive` on the `PostEventParticipant` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "PostEventParticipant" DROP COLUMN "isActive";
