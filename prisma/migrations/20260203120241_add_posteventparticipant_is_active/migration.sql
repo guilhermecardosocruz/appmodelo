7cdf10fe92d5986af36d7f9b1d24810248617eeb
guilherme <guilherme.cardosocruz@hotmail.com>
2026-02-04 17:02:41 -0300

    Chore: remove duplicated PostEventParticipant isActive migrations

--- prisma/migrations/20260203120241_add_posteventparticipant_is_active/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `isActive` on the `PostEventParticipant` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "PostEventParticipant" DROP COLUMN "isActive";
--- prisma/migrations/20260203180000_add_posteventparticipant_is_active/migration.sql
+++
@@ -1,3 +0,0 @@
--- Adiciona coluna de status (ativo/inativo) para participantes de racha
-ALTER TABLE "PostEventParticipant"
-ADD COLUMN "isActive" BOOLEAN NOT NULL DEFAULT true;
